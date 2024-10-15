e577855edb7e300badeb25470e0be0e188540f68
YoshithMaddi <yoshithmy23@gmail.com>
2024-10-15 18:49:20 +0530

    working_on hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <img src={image} alt="" />
       <h1> <span>I'm Yoshith</span>, frontend developer based in India.</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cupiditate labore iusto perferendis consequuntur perspiciatis sint aperiam, provident unde nobis quam assumenda?</p>
-      <div className="hero-actions">
+      <div className="hero-action">
         <div className="connect">Connect with me</div>
         <div className="my-resume">MY resume</div>
       </div>
